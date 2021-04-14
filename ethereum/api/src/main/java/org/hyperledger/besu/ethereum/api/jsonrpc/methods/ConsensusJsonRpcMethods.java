0e0a61b2ee17423044404a94bc98c19219ec9e4a
garyschulte <garyschulte@gmail.com>
2021-04-14 16:22:08 -0700

    add rpc method stubs

--- ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/ConsensusJsonRpcMethods.java
+++ ethereum/api/src/main/java/org/hyperledger/besu/ethereum/api/jsonrpc/methods/ConsensusJsonRpcMethods.java
@@ -2,9 +2,12 @@ package org.hyperledger.besu.ethereum.api.jsonrpc.methods;
 
 import org.hyperledger.besu.ethereum.api.jsonrpc.RpcApi;
 import org.hyperledger.besu.ethereum.api.jsonrpc.RpcApis;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.ConsensusAssembleBlock;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.ConsensusFinalizeBlock;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.ConsensusNewBlock;
+import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.ConsensusSetHead;
 import org.hyperledger.besu.ethereum.api.jsonrpc.internal.methods.JsonRpcMethod;
 
-import java.util.Collections;
 import java.util.Map;
 
 public class ConsensusJsonRpcMethods extends ApiGroupJsonRpcMethods {
@@ -15,6 +18,10 @@ public class ConsensusJsonRpcMethods extends ApiGroupJsonRpcMethods {
 
   @Override
   protected Map<String, JsonRpcMethod> create() {
-    return Collections.emptyMap();
+    return mapOf(
+        new ConsensusAssembleBlock(),
+        new ConsensusFinalizeBlock(),
+        new ConsensusNewBlock(),
+        new ConsensusSetHead());
   }
 }
