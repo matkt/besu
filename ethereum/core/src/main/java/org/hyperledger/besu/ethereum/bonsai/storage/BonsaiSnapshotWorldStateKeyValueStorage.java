2aee8fa7a74450b356cdad2c973891f6e33dfc4c
Karim TAAM <karim.t2am@gmail.com>
2023-03-09 17:12:15 +0100

    close correctly persisted storage
    Signed-off-by: Karim TAAM <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/storage/BonsaiSnapshotWorldStateKeyValueStorage.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/bonsai/storage/BonsaiSnapshotWorldStateKeyValueStorage.java
@@ -15,29 +15,14 @@
  */
 package org.hyperledger.besu.ethereum.bonsai.storage;
 
-import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.ethereum.bonsai.storage.BonsaiWorldStateKeyValueStorage.BonsaiStorageSubscriber;
 import org.hyperledger.besu.plugin.services.exception.StorageException;
 import org.hyperledger.besu.plugin.services.storage.KeyValueStorage;
 import org.hyperledger.besu.plugin.services.storage.SnappableKeyValueStorage;
 import org.hyperledger.besu.plugin.services.storage.SnappedKeyValueStorage;
 
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Supplier;
-
-import org.apache.tuweni.bytes.Bytes;
-import org.apache.tuweni.bytes.Bytes32;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class BonsaiSnapshotWorldStateKeyValueStorage extends BonsaiWorldStateKeyValueStorage
     implements BonsaiStorageSubscriber {
-
-  private static final Logger LOG =
-      LoggerFactory.getLogger(BonsaiSnapshotWorldStateKeyValueStorage.class);
-  private final AtomicBoolean shouldClose = new AtomicBoolean(false);
-  private final AtomicBoolean isClosed = new AtomicBoolean(false);
 
   protected final BonsaiWorldStateKeyValueStorage parentWorldStateStorage;
 
@@ -77,71 +62,6 @@ public class BonsaiSnapshotWorldStateKeyValueStorage extends BonsaiWorldStateKey
   }
 
   @Override
-  public Optional<Bytes> getAccount(final Hash accountHash) {
-    return isClosed.get() ? Optional.empty() : super.getAccount(accountHash);
-  }
-
-  @Override
-  public Optional<Bytes> getCode(final Bytes32 codeHash, final Hash accountHash) {
-    return isClosed.get() ? Optional.empty() : super.getCode(codeHash, accountHash);
-  }
-
-  @Override
-  public Optional<Bytes> getAccountStateTrieNode(final Bytes location, final Bytes32 nodeHash) {
-    return isClosed.get() ? Optional.empty() : super.getAccountStateTrieNode(location, nodeHash);
-  }
-
-  @Override
-  public Optional<Bytes> getAccountStorageTrieNode(
-      final Hash accountHash, final Bytes location, final Bytes32 nodeHash) {
-    return isClosed.get()
-        ? Optional.empty()
-        : super.getAccountStorageTrieNode(accountHash, location, nodeHash);
-  }
-
-  @Override
-  public Optional<byte[]> getTrieLog(final Hash blockHash) {
-    return isClosed.get() ? Optional.empty() : super.getTrieLog(blockHash);
-  }
-
-  @Override
-  public Optional<Bytes> getStateTrieNode(final Bytes location) {
-    return isClosed.get() ? Optional.empty() : super.getStateTrieNode(location);
-  }
-
-  @Override
-  public Optional<Bytes> getWorldStateRootHash() {
-    return isClosed.get() ? Optional.empty() : super.getWorldStateRootHash();
-  }
-
-  @Override
-  public Optional<Hash> getWorldStateBlockHash() {
-    return isClosed.get() ? Optional.empty() : super.getWorldStateBlockHash();
-  }
-
-  @Override
-  public Optional<Bytes> getStorageValueBySlotHash(final Hash accountHash, final Hash slotHash) {
-    return isClosed.get()
-        ? Optional.empty()
-        : super.getStorageValueBySlotHash(accountHash, slotHash);
-  }
-
-  @Override
-  public Optional<Bytes> getStorageValueBySlotHash(
-      final Supplier<Optional<Hash>> storageRootSupplier,
-      final Hash accountHash,
-      final Hash slotHash) {
-    return isClosed.get()
-        ? Optional.empty()
-        : super.getStorageValueBySlotHash(storageRootSupplier, accountHash, slotHash);
-  }
-
-  @Override
-  public boolean isWorldStateAvailable(final Bytes32 rootHash, final Hash blockHash) {
-    return isClosed.get() ? false : super.isWorldStateAvailable(rootHash, blockHash);
-  }
-
-  @Override
   public void clear() {
     // snapshot storage does not implement clear
     throw new StorageException("Snapshot storage does not implement clear");
@@ -160,30 +80,29 @@ public class BonsaiSnapshotWorldStateKeyValueStorage extends BonsaiWorldStateKey
   }
 
   @Override
-  public synchronized long subscribe(final BonsaiStorageSubscriber sub) {
-    if (isClosed.get()) {
-      throw new RuntimeException("Storage is marked to close or has already closed");
-    }
-    return super.subscribe(sub);
-  }
+  protected synchronized void doClose() throws Exception {
+    if (!isClosed.get()) {
+      // alert any subscribers we are closing:
+      subscribers.forEach(BonsaiStorageSubscriber::onCloseStorage);
 
-  @Override
-  public synchronized void unSubscribe(final long id) {
-    super.unSubscribe(id);
-    try {
-      tryClose();
-    } catch (Exception e) {
-      LOG.atWarn()
-          .setMessage("exception while trying to close : {}")
-          .addArgument(e::getMessage)
-          .log();
+      // close all of the SnappedKeyValueStorages:
+      accountStorage.close();
+      codeStorage.close();
+      storageStorage.close();
+      trieBranchStorage.close();
+
+      // unsubscribe the parent worldstate
+      parentWorldStateStorage.unSubscribe(subscribeParentId);
+
+      // set storage closed
+      isClosed.set(true);
     }
   }
 
   @Override
   public void onCloseStorage() {
     try {
-      // when the parent storage clears, close regardless of subscribers
+      // when the parent storage close, close regardless of subscribers
       doClose();
     } catch (Exception e) {
       throw new RuntimeException(e);
@@ -217,39 +136,6 @@ public class BonsaiSnapshotWorldStateKeyValueStorage extends BonsaiWorldStateKey
       doClose();
     } catch (Exception e) {
       throw new RuntimeException(e);
-    }
-  }
-
-  @Override
-  public synchronized void close() throws Exception {
-    // when the parent storage clears, close
-    shouldClose.set(true);
-    tryClose();
-  }
-
-  protected synchronized void tryClose() throws Exception {
-    if (shouldClose.get() && subscribers.getSubscriberCount() < 1) {
-      // attempting to close already closed snapshots will segfault
-      doClose();
-    }
-  }
-
-  private synchronized void doClose() throws Exception {
-    if (!isClosed.get()) {
-      // alert any subscribers we are closing:
-      subscribers.forEach(BonsaiStorageSubscriber::onCloseStorage);
-
-      // close all of the SnappedKeyValueStorages:
-      accountStorage.close();
-      codeStorage.close();
-      storageStorage.close();
-      trieBranchStorage.close();
-
-      // unsubscribe the parent worldstate
-      parentWorldStateStorage.unSubscribe(subscribeParentId);
-
-      // set storage closed
-      isClosed.set(true);
     }
   }
 
