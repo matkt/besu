a12873da1c7acc30889c32362071d7f2b49dc123
Karim Taam <karim.t2am@gmail.com>
2025-07-31 15:02:57 +0400

    display timestamp
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/transition/StateTransitionWorldState.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/transition/StateTransitionWorldState.java
@@ -80,6 +80,7 @@ public class StateTransitionWorldState implements MutableWorldState, PathBasedWo
   public void announceBlockToImport(final BlockHeader blockToImport) {
     // Determine if BinTrie should be activated based on the fork timestamp.
     this.isBinTrieActive = blockToImport.getTimestamp() >= verkleForkTimeStamp;
+    System.out.println(blockToImport.getTimestamp()+" "+verkleForkTimeStamp);
     // Load the appropriate state accumulator.
     this.accumulator = loadAccumulator();
   }
