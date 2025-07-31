de2cfc2f184397ffed98ab8ee666a7b7aa62e223
Karim Taam <karim.t2am@gmail.com>
2025-07-31 19:16:53 +0400

    add fallback log
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/transition/StateTransitionWorldState.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/transition/StateTransitionWorldState.java
@@ -151,11 +151,14 @@ public class StateTransitionWorldState implements MutableWorldState, PathBasedWo
         // diff based value
         maybeSlot.ifPresent(
             slot -> {
+              System.out.println("fallback "+address+" "+storageSlotKey.getSlotKey().get()+" "+slot);
               verkleWorldState
                   .getAccumulator()
                   .maybeCreateStorageMap(address)
                   .put(storageSlotKey, new MigratedDiffValue<>(slot, slot));
             });
+      }  else {
+        System.out.println("found "+address+" "+storageSlotKey.getSlotKey().get()+" "+maybeSlot.get());
       }
       return maybeSlot;
     } else {
