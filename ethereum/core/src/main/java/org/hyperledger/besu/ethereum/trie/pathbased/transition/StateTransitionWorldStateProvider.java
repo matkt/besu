285cacdbe47ab24506f25840c7faa79bbcac02d5
Karim Taam <karim.t2am@gmail.com>
2025-08-08 11:37:06 +0400

    move from 1500 to 7000
    Signed-off-by: Karim Taam <karim.t2am@gmail.com>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/transition/StateTransitionWorldStateProvider.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/transition/StateTransitionWorldStateProvider.java
@@ -86,7 +86,7 @@ public class StateTransitionWorldStateProvider implements WorldStateArchive {
             .getTrieLogLayer(params.getBlockHash())
             .filter(log -> log.getDataStorageFormat() == DataStorageFormat.BINTRIE)
             .flatMap(TrieLog::getStateMigrationLog)
-            .orElse(new StateMigrationLog(params.getBlockHash(), 1500));
+            .orElse(new StateMigrationLog(params.getBlockHash(), 7000));
 
     final BlockHeader bonsaiTarget =
         blockchain.getBlockHeader(migrationLog.getFirstBlockHash()).orElseThrow();
@@ -144,7 +144,7 @@ public class StateTransitionWorldStateProvider implements WorldStateArchive {
             .getTrieLogManager()
             .getTrieLogLayer(chainHeadHeader.getBlockHash())
             .flatMap(TrieLog::getStateMigrationLog)
-            .orElse(new StateMigrationLog(chainHeadHeader.getBlockHash(), 1500));
+            .orElse(new StateMigrationLog(chainHeadHeader.getBlockHash(), 700));
 
     LOG.debug(
         "State migration log retrieved for chain head block hash: {}",
