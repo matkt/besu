edc22e321657ec21274b06948242deebb470eb01
Ameziane H. <ameziane.hamlat@consensys.net>
2026-08-31 12:45:25 +0200

    Use conditional to avoid any execution
    Signed-off-by: Ameziane H. <ameziane.hamlat@consensys.net>

--- evm/src/main/java/org/hyperledger/besu/evm/operation/SStoreOperation.java
+++ evm/src/main/java/org/hyperledger/besu/evm/operation/SStoreOperation.java
@@ -126,16 +126,16 @@ public class SStoreOperation extends AbstractOperation {
         gasCalculator()
             .calculateStorageRefundAmount(newValue, currentValueSupplier, originalValueSupplier));
 
-    LOG.atTrace()
-        .setMessage(
-            "EIP-8037 REC_STORAGE depth={} addr={} key={} txEntryIsZero={} beforeIsZero={} afterIsZero={}")
-        .addArgument(frame::getDepth)
-        .addArgument(address::toHexString)
-        .addArgument(() -> "0x" + key.toHexString().substring(2))
-        .addArgument(() -> originalValueSupplier.get().isZero())
-        .addArgument(() -> currentValueSupplier.get().isZero())
-        .addArgument(newValue::isZero)
-        .log();
+    if (LOG.isTraceEnabled()) {
+      LOG.trace(
+          "EIP-8037 REC_STORAGE depth={} addr={} key={} txEntryIsZero={} beforeIsZero={} afterIsZero={}",
+          frame.getDepth(),
+          address.toHexString(),
+          "0x" + key.toHexString().substring(2),
+          originalValueSupplier.get().isZero(),
+          currentValueSupplier.get().isZero(),
+          newValue.isZero());
+    }
 
     final StateGasCostCalculator stateGasCalc = gasCalculator().stateGasCostCalculator();
     final StorageTransition transition =
